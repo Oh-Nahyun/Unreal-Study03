ff607bd7d12574a616a88c37483695b4781c9adb
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2025-01-21 19:41:42 +0900

    Update main.cpp

--- Game/02_SpaceShip/main.cpp
+++ Game/02_SpaceShip/main.cpp
@@ -30,7 +30,7 @@
 // 함수 정의 ------------------------------
 
 void Intro_game();
-void Ending_game();
+void Ending_game(int num);
 
 void gotoxy(int x, int y);
 void Print_SpaceShip(int x, int y);
@@ -60,29 +60,36 @@ int main()
 	//system("color e");
 	system("color 9e");		// 첫자리 : 배경색, 두번째 : 글자색
 
+	// 화면 사이즈 & 제목 조정 
+	system("cls");
+	system("mode con:cols=75 lines=30");
+	system("title SpaceShip");
+
 	// 착륙 지점 랜덤 처리
 	srand((unsigned)time(NULL));
 
 	// 시작 지점 x값 설정
 	// (우주선 시작 랜덤값)
-	int x = 0;
+	int ShipX = 0;
 	do {
-		x = rand() % 30;
-	} while (x % 2 == 0);
+		ShipX = rand() % 30;
+	} while (ShipX % 2 == 0);
 
 	// 0. 테스트 ------------------------------
 
 	// Draw Test : 캐릭터, 액터(mob), 오브젝트 출력
-	Print_SpaceShip(1, 1);
-	Print_Platform();
+	//Print_SpaceShip(1, 1);	Sleep(2000); Del_SpaceShip(1, 1);
+	//Print_Platform();			system("pause");
+	///// ??? ///// printf("A\n");
 
 	// 1. 게임 인트로 ------------------------------
 	Intro_game();
 
 	// 2. 게임 플레이 & 콘트롤 ------------------------------
-	Game_Control(x);
+	Game_Control(ShipX);
 
 	// 3. 게임 종료 ------------------------------
+	gotoxy(20, 28);
 	system("pause");
 
 	return 0;
@@ -95,15 +102,51 @@ int main()
 /// </summary>
 void Intro_game()
 {
+	system("cls");
 
+	printf("\t\t \n\n");
+	printf("\t\t ========== 우주선 착륙 게임 ==========\n\n");
+	printf("\t\t    우주선을 착륙시키는 게임입니다.\n\n");
+	printf("\t\t  우주선은 화면 위에 위치하는데\n\n");
+	printf("\t\t  좌, 우 화살표 키를 이용하여\n\n");
+	printf("\t\t  착륙 지점에 정확히 착륙시켜야 합니다.\n\n");
+	printf("\t\t \n\n");
+	printf("\t\t  우주선 모양\t\t착륙 위치\n\n");
+	printf("\t\t      ▲\n\n");
+	printf("\t\t    ▲▲▲\n\n");
+	printf("\t\t    ▲  ▲\t\t □□□\n\n");
+	printf("\t\t \n\n");
+	printf("\t\t     < 아무 키나 누르면 시작합니다. > \n\n");
+
+	gotoxy(20, 27);
+	system("pause");
+	system("cls");
 }
 
 /// <summary>
 /// 결과 출력 함수
 /// </summary>
-void Ending_game()
+void Ending_game(int num)
 {
+	switch (num)
+	{
+		case 1:		// 성공인 경우
+			gotoxy(1, 27);
+			printf("\t ========== 착륙에 성공하였습니다! ==========");
+			break;
+		case 2:		// 절반의 성공인 경우
+			gotoxy(1, 27);
+			printf("\t ===== 아쉽게도 완벽한 착륙에는 성공하지 못하였습니다! =====");
+			break;
+		case 3:		// 실패인 경우
+			gotoxy(1, 27);
+			printf("\t ========== 착륙에 실패하였습니다! ==========");
+			break;
+	}
 
+	//gotoxy(20, 27);
+	//system("pause");
+	//system("cls");
 }
 
 /// <summary>
@@ -111,7 +154,8 @@ void Ending_game()
 /// </summary>
 void gotoxy(int x, int y)
 {
-
+	COORD Pos = { x - 1, y - 1 };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 }
 
 /// <summary>
@@ -119,42 +163,138 @@ void gotoxy(int x, int y)
 /// </summary>
 void Print_SpaceShip(int x, int y)
 {
+	int count = 1;
+	gotoxy(x, y);
 
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			if (Spaceship[i][j])
+			{
+				printf("▲");
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		gotoxy(x, y + count++);
+	}
 }
 
 /// <summary>
 /// 우주선 지우는 함수
+/// (Windows 방식)
 /// </summary>
 void Del_SpaceShip(int x, int y)
 {
+	int count = 1;
+	gotoxy(x, y);
 
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			printf("  ");
+		}
+		gotoxy(x, y + count++);
+	}
 }
 
 /// <summary>
 /// 우주선 이동키 (좌, 우) 함수
+/// (화살표 키를 이용한 문자의 이동 함수)
 /// </summary>
 void Move_ArrowKey(char key, int* x1, int x_b)
 {
-
+	switch (key)
+	{
+		case 75:	// 왼쪽 화살표 키
+			*x1 = *x1 - 2;
+			if (*x1 < 1) *x1 = 1;		// 화면을 벗어나지 않도록 설정
+			Beep(1318.510, 10);			// #define _E 1318.510
+			break;
+		case 77:	// 오른쪽 화살표 키
+			*x1 = *x1 + 2;
+			if (*x1 > x_b) *x1 = x_b;	// 화면을 벗어나지 않도록 설정
+			Beep(1396.913, 10);			// #define _F 1396.913
+			break;
+		default:
+			break;
+	}
 }
 
 /// <summary>
 /// 우주선 아래로 이동시키는 함수
 /// </summary>
-/// <returns></returns>
+/// <returns>우주선의 이동 위치</returns>
 int Move_Down(int x)
 {
-	return 0;
+	int y = 1;
+	char key;
+
+	for (y = 1; y < 20; y++)
+	{
+		gotoxy(x, y);
+
+		// 우주선 출력
+		Print_SpaceShip(x, y);
+		Sleep(150);
+
+		// 우주선 삭제
+		//system("cls");		// 전체 화면을 지우는 방식 ==> 플랫폼이 지워지는 에러 발생
+		Del_SpaceShip(x, y);	// 우주선 부분만 지우는 방식
+
+		// 키보드 입력받기
+		if (_kbhit())
+		{
+			key = _getch();
+			Move_ArrowKey(key, &x, 30);
+		}
+
+		// 게임 중간 출력 (디버그)
+		gotoxy(14, 25);
+		printf("우주선 : %d", x);
+	}
+
+	// 최종 위치
+	gotoxy(x, y);
+
+	// 우주선 다시 출력
+	Print_SpaceShip(x, y);
+
+	// 우주선의 이동 위치 반환
+	return x;
 }
 
 /// <summary>
 /// 도착점 출력 함수
 /// (위치는 랜덤)
 /// </summary>
-/// <returns></returns>
+/// <returns>착륙장 시작 위치</returns>
 int Print_Platform()
 {
-	return 0;
+	// 위치
+	int number = 0;
+	do {
+		number = rand() % 30;
+	} while (number % 2 == 0);
+
+	// 커서 이동
+	gotoxy(number, 23);
+
+	// 출력
+	for (int i = 0; i < 3; i++)
+	{
+		if (Platform[i])
+		{
+			printf("□");
+		}
+	}
+
+	// 착륙장 시작 위치 반환 (결과 확인용)
+	return number;
 }
 
 /// <summary>
@@ -164,5 +304,39 @@ int Print_Platform()
 /// <returns></returns>
 int Game_Control(int x)
 {
+	// 1. 착륙 위치 출력
+	// 2. 우주선 출력 & 이동 & 하강
+	// 3. 게임의 결과 처리
+
+	// 기준 좌표값
+	int result = 0;		// 플랫폼[0] = 맨 왼쪽 x값 □□□
+	int sResult = 0;	// 우주선[0] = 맨 왼쪽 x값 ▲  ▲
+
+	// 플랫폼 출력
+	result = Print_Platform();
+
+	// 게임 결과 출력 (확인)
+	gotoxy(1, 25);
+	printf("도착점 : %d", result);
+
+	// 내려오는 우주선 출력
+	sResult = Move_Down(x);
+
+	// 게임 결과 출력 (재확인)
+	gotoxy(1, 25);
+	printf("도착점 : %d, 우주선 : %d", result, sResult);
+
+	// 결과 출력
+	int GameResult = 0;
+
+	if (sResult == result)
+		GameResult = 1;	// 성공
+	else if (sResult + 2 == result || sResult + 4 == result || sResult - 2 == result || sResult - 4 == result)
+		GameResult = 2;	// 걸친 상태
+	else
+		GameResult = 3; // 실패
+
+	Ending_game(GameResult);
+
 	return 0;
 }
