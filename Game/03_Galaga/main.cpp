d31ff52e4f48cb6ef54ea0ec00b0ddd5700ef3ae
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2025-01-22 19:51:05 +0900

    Update main.cpp

--- Game/03_Galaga/main.cpp
+++ Game/03_Galaga/main.cpp
@@ -17,8 +17,350 @@
 
 /// 게임이 아닌, 프로토타입!! (조작 및 게임의 형태만 구축할 예정)
 
-/// 필요 함수, 라이브러리 체크 ==============================
-
-/// 
-
 /// ==================================================
+
+#include <stdio.h>		// 기본 출력 함수 (C Base)
+#include <Windows.h>	// 커서 관련 (콘솔)
+#include <time.h>		// 시간
+#include <stdlib.h>		// 랜덤 etc...
+#include <conio.h>		// (콘솔의) 출력
+
+// 데이터 정의 ------------------------------
+
+// 좌표 (디파인)
+#define BOSS_X 12		// 보스의 기본 좌표
+#define BOSS_Y 7
+#define USER_X 14		// 유저의 기본 좌표
+#define USER_Y 40
+
+// 총알
+typedef struct bullet
+{
+	int x, y;
+};
+
+// 총알 총 수량 (화면에 출력되는 최대 숫자)
+struct
+{
+	BOOL exist;
+	int x, y;
+} Bullet[3];
+
+// 플레이어 데이터
+typedef struct user_stat
+{
+	int x, y;			// 위치
+	int life;			// 생명
+	int chk;			// 총알의 발사 여부
+	bullet bullet;		// 각각의 총알
+} user_stat;
+
+// 보스 데이터
+typedef struct boss_stat
+{
+	int x, y;			// 위치
+	int life;			// 생명
+	int chk;			// 총알의 발사 여부
+	bullet bullet;		// 각각의 총알
+} boss_stat;
+
+// 적 데이터
+// ...
+
+// 객체(플레이어, 보스) 데이터 선언
+struct user_stat User;
+struct boss_stat Boss;
+
+// 함수 정의 ------------------------------
+
+void gotoxy(int x, int y);
+void cursor(int n);
+
+void boss(int x, int y);
+void bossDelete(int x, int y);
+void player(int x, int y);
+void playerDelete(int x, int y);
+
+void bulletMove();
+void DrawBullet(int i);
+void EraseBullet(int i);
+
+void menu();
+void game();
+void help();
+
+/*
+menu() = UI 처리, 게임 로비
+{
+	game();		// 플레이 함수
+	help();		// 도움말
+	exit();		// 종료
+}
+
+game()
+{
+	// 보스 출력, 랜덤 좌우 이동
+	// 플레이어 출력, 조작 이동 (상하좌우), 공격 (space key)
+	// 총알 발사 처리
+	// 총알 이동 처리
+}
+*/
+
+// Main 함수 구현 ------------------------------
+
+int main()
+{
+	// 시스템 처리
+	// cursor(true);	// (커서 처리 : 보이도록)
+	cursor(false);		// (커서 처리 : 안보이도록)
+
+	// 랜덤 초기화
+	srand((unsigned)time(NULL));
+
+	// 메뉴 (게임 플레이 본체) & 화면 초기화
+	menu();
+
+	// 게임 종료 (test:일시 정지)
+	system("pause");
+
+	return 0;
+}
+
+// 함수 구현 ------------------------------
+
+/// <summary>
+/// 커서 이동 함수
+/// </summary>
+/// <param name="x"></param>
+/// <param name="y"></param>
+void gotoxy(int x, int y)
+{
+	COORD Pos = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+}
+
+/// <summary>
+/// 커서를 숨기거나 보이도록 처리하는 함수
+/// </summary>
+/// <param name="n">커서 보이기 = 1 (true), 커서 숨기기 = 0 (false)</param>
+void cursor(int n)
+{
+	HANDLE hConsole;
+	CONSOLE_CURSOR_INFO ConsoleCursor;
+
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	ConsoleCursor.bVisible = n;
+	ConsoleCursor.dwSize = 1;		// 숫자가 커질 수록 커서가 커진다.
+
+	SetConsoleCursorInfo(hConsole, &ConsoleCursor);
+}
+
+/// <summary>
+/// 보스 출력 함수
+/// </summary>
+/// <param name="x"></param>
+/// <param name="y"></param>
+void boss(int x, int y)
+{
+	gotoxy(x	, y);		printf("■■      ■■");
+	gotoxy(x + 4, y + 1);	printf("■■■");
+	gotoxy(x + 2, y + 2);	printf("■■■■■");
+	gotoxy(x	, y + 3);	printf("■  ■■■  ■");
+	gotoxy(x - 2, y + 4);	printf("■■■■■■■■■");
+	gotoxy(x	, y + 5);	printf("■■      ■■");
+	gotoxy(x + 2, y + 6);	printf("■■  ■■");
+}
+
+/// <summary>
+/// 보스 지우는 함수
+/// </summary>
+/// <param name="x"></param>
+/// <param name="y"></param>
+void bossDelete(int x, int y)
+{
+
+	gotoxy(x	, y);		printf("              ");
+	gotoxy(x + 4, y + 1);	printf("      ");
+	gotoxy(x + 2, y + 2);	printf("          ");
+	gotoxy(x	, y + 3);	printf("              ");
+	gotoxy(x - 2, y + 4);	printf("                  ");
+	gotoxy(x	, y + 5);	printf("              ");
+	gotoxy(x + 2, y + 6);	printf("          ");
+}
+
+/// <summary>
+/// 플레이어 출력 함수
+/// </summary>
+/// <param name="x"></param>
+/// <param name="y"></param>
+void player(int x, int y)
+{
+	gotoxy(x	, y);		printf("▲");
+	gotoxy(x - 2, y + 1);	printf("▲■▲");
+	gotoxy(x - 2, y + 2);	printf("◎  ◎");
+}
+
+/// <summary>
+/// 플레이어 지우는 함수
+/// </summary>
+/// <param name="x"></param>
+/// <param name="y"></param>
+void playerDelete(int x, int y)
+{
+	gotoxy(x	, y);		printf("  ");
+	gotoxy(x - 2, y + 1);	printf("      ");
+	gotoxy(x - 2, y + 2);	printf("      ");
+}
+
+/// <summary>
+/// 유저의 총알 이동 함수
+/// </summary>
+void bulletMove()
+{
+
+}
+
+/// <summary>
+/// 유저의 총알 출력 함수
+/// </summary>
+/// <param name="i"></param>
+void DrawBullet(int i)
+{
+
+}
+
+/// <summary>
+/// 유저의 총알 지우는 함수
+/// </summary>
+/// <param name="i"></param>
+void EraseBullet(int i)
+{
+
+}
+
+/// <summary>
+/// 메뉴 함수
+/// (게임 메뉴 선택 : 로비 (1. 게임시작 / 2. 게임방법 / 3. 게임종료))
+/// </summary>
+void menu()
+{
+	// 메뉴 선택 번호
+	int select = 0;
+
+	// 화면 비율 변경
+	system("mode CON COLS=60 LINES=50");	// Windows11은 다르다.
+	system("color 1e");						// 첫자리 : 배경색, 두번째 : 글자색 (0 ~ f)
+
+	// 메뉴 반복 부분
+	while (true)
+	{
+		// 메뉴 출력
+		system("cls");
+		gotoxy(16, 7);	printf("******************************");
+		gotoxy(16, 8);	printf("*                            *");
+		gotoxy(16, 9);	printf("*       1. 게임 시작         *");
+		gotoxy(16, 10);	printf("*                            *");
+		gotoxy(16, 11);	printf("*       2. 게임 방법         *");
+		gotoxy(16, 12);	printf("*                            *");
+		gotoxy(16, 13);	printf("*       3. 게임 종료         *");
+		gotoxy(16, 14);	printf("*                            *");
+		gotoxy(16, 15);	printf("******************************");
+
+		gotoxy(22, 35);	printf("번호를 입력하세요. : ");
+		scanf_s("%d", &select);
+
+		// 메뉴 선택
+		if (select == 1)
+		{
+			gotoxy(20, 37);
+			printf("게임을 시작하겠습니다.");
+			Sleep(1000);
+
+			game();
+		}
+		else if (select == 2)
+		{
+			help();
+		}
+		else
+		{
+			gotoxy(20, 37);
+			printf("게임을 종료하겠습니다.");
+			Sleep(1000);
+			gotoxy(15, 39);		// pause 위치..
+
+			break;
+		}
+	}
+}
+
+/// <summary>
+/// 게임 실행 부분 (게임 매니저) 함수
+/// </summary>
+void game()
+{
+	// 화면 초기화
+	system("cls");
+
+	// 데이터 초기화
+	User.x = USER_X;
+	User.y = USER_Y;
+	User.chk = 0;
+
+	Boss.x = BOSS_X;
+	Boss.y = BOSS_Y;
+	Boss.chk = 0;			// 보스는 화면에 1발만 처리 (총알이 화면을 벗어나면 발사)
+	Boss.bullet.y = 0;
+
+	// 시간 기록용 변수
+	// (((타이머 (스레드) : 컴퓨터마다 1초의 시간이 달라지면 안되기 때문)))
+
+	// 보스 이동 플래그 (좌우 이동)
+
+	// 총알 인덱스
+
+	// 게임 반복 구간
+	while (true)
+	{
+		// 게임 종료 (ESC Key)
+		if (GetAsyncKeyState(VK_ESCAPE) & 0x01) break;
+
+		// 보스, 플레이어 출력
+		boss(Boss.x * 2, Boss.y);
+		player(User.x * 2, User.y);
+
+		// 총알 처리 & 출력
+
+		// 입력 처리 (플레이어 이동 처리 : 상하좌우)
+
+		// 유저 총알 쏘는 부분 (Space Key)
+
+		// 보스 처리 (1초마다 처리 (NPC))
+
+		// 게임 딜레이 (전체적인 게임 속도)
+		Sleep(10);
+	}
+}
+
+/// <summary>
+/// 게임 설명 (도움말) 함수
+/// </summary>
+void help()
+{
+	system("cls");
+
+	// 보스 출력
+	boss(BOSS_X * 2, BOSS_Y);
+
+	// 유저 출력
+	player(USER_X * 2, USER_Y);
+
+	// 설명 출력
+	gotoxy(19, 17);	printf("위와 같은 보스가 나타나면");
+	gotoxy(7, 19);	printf("↑,↓,←,→ 방향키를 이용하여 적의 공격을 피하고");
+	gotoxy(11, 21);	printf("스페이스 키를 이용하여 적을 공격하세요.");
+
+	gotoxy(13, 25);	system("pause");
+
+}
