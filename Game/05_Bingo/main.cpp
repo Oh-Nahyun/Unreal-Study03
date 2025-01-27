5da79758f782d0995d023c017ce75f34ad86ff2c
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2025-01-27 19:50:23 +0900

    Update main.cpp

--- Game/05_Bingo/main.cpp
+++ Game/05_Bingo/main.cpp
@@ -5,14 +5,19 @@
 
 void BingoGameStart();             // Bingo 게임 함수 = play();
 
-// 컴파일러를 위한 선언...
+/// 컴파일러를 위한 선언... (개인 개발 시 사용)
 void Init(int*);                   // 초기화
 void PrintBingoBoard(int*, int);   // Bingo 판 그리기
 void InputNum(int*);               // 입력 처리
 int CheckBingo(int*);              // Bingo 게임 결과 처리 부분 (!!)
 
-// 사람(동료)을 위한 선언...
+/// 사람을 위한 선언... (동료와 함께 개발 시 사용 (추천!!!))
 //void Init(int* bingo);
 //void PrintBingoBoard(int* bingo, int count);
 //void InputNum(int* bingo);
 //int CheckBingo(int* bingo);
+
+int main()
+{
+  return 0;
+}
